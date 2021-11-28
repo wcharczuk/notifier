854e142ca3663119c04b8d9e40ad8cfcc553ef8c
Will Charczuk <wcharczuk@users.noreply.github.com>
2021-11-27 20:12:10 -0800

    updates

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"flag"
 	"os"
+	"sync"
 
 	"github.com/blend/go-sdk/ansi/slant"
+	"github.com/blend/go-sdk/async"
 	"github.com/blend/go-sdk/configutil"
 	"github.com/blend/go-sdk/logger"
 
@@ -30,26 +32,44 @@ func main() {
 	slant.Print(os.Stdout, "LaMETRIC")
 
 	log := logger.All()
-	client := lametric.New(cfg.Devices[0].Addr, cfg.Devices[0].Token)
-	client.Client.Log = log
-	_, err := client.CreateNotification(context.Background(), lametric.Notification{
+
+	notification := lametric.Notification{
 		Model: lametric.NotificationModel{
 			Frames: []lametric.Frame{
 				{
 					Icon: lametric.IconAttention,
 					Text: "ALERT",
 				},
-				{
-					Icon: lametric.IconAppleLogo,
-					Text: "Adam has been fired.",
+			},
+			/*
+				Sound: &lametric.Sound{
+					Category: lametric.SoundCategoryAlarms,
+					ID:       lametric.SoundAlarm10,
 				},
-			},
-			Sound: &lametric.Sound{
-				Category: lametric.SoundCategoryAlarms,
-				ID:       lametric.SoundAlarm10,
-			},
+			*/
 		},
-	})
-	logger.MaybeError(log, err)
-	// FINISH.
+	}
+
+	errs := make(async.Errors, len(cfg.Devices))
+	wg := sync.WaitGroup{}
+	wg.Add(len(cfg.Devices))
+	for x := 0; x < len(cfg.Devices); x++ {
+		go func(index int) {
+			defer wg.Done()
+			if err := send(context.Background(), cfg.Devices[index], notification); err != nil {
+				errs <- err
+			}
+		}(x)
+	}
+	wg.Wait()
+	if err := errs.All(); err != nil {
+		logger.MaybeFatalExit(log, errs.All())
+	}
+	logger.MaybeInfof(log, "%d notifications sent", len(cfg.Devices))
+}
+
+func send(ctx context.Context, device config.Device, notification lametric.Notification) error {
+	client := lametric.New(device.Addr, device.Token)
+	_, err := client.CreateNotification(context.Background(), notification)
+	return err
 }
